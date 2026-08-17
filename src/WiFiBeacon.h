538ef6abe477f356d15c7ff908559e9510bccbe6
egraich <egorbabchuk1@gmail.com>
2026-08-17 04:52:39 +0300

    feat: final beacons feature

---
+++ src/WiFiBeacon.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <Arduino.h>
+
+void setupWiFiBeacons();
+void spamBeacons();
--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,25 @@
 #include <WiFi.h>
 #include "Config.h"
 #include "TelegramBot.h"
+#include "WiFiBeacon.h"
 
 void setup() {
     Serial.begin(115200);
     
-    // 1. Подключение к Wi-Fi (получаем интернет с телефона)
-    WiFi.mode(WIFI_STA); 
+    // ВАЖНО: Включаем режим AP+STA
+    WiFi.mode(WIFI_AP_STA); 
     WiFi.begin(WIFI_SSID, WIFI_PASS);
+    
     Serial.print("Подключаюсь к Wi-Fi");
     while (WiFi.status() != WL_CONNECTED) { delay(500); Serial.print("."); }
-    Serial.println("\n[SYSTEM] Wi-Fi подключен! IP: " + WiFi.localIP().toString());
+    Serial.println("\n[SYSTEM] Wi-Fi подключен!");
     
     setupTelegramBot();
-    
+    setupWiFiBeacons();
 }
 
 void loop() {
-    // Крутим бота
     tickTelegramBot();
     
-    // В будущем здесь будет: spamBeacons();
+    spamBeacons(); // Рассылаем пакеты в эфир раз в 100 мс!
 }
